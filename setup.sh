38fcd87be3c66223d52d0d092bd2dbfed417715f
Steven Addison <92377177+stevenaddison@users.noreply.github.com>
2022-01-24 11:14:37 -0800

    updating to include theme

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+echo "
+[theme]
+primaryColor="#f9b36b"
+backgroundColor="#f3e9ce"
+secondaryBackgroundColor="#f9b36b"
+textColor="#000000"
+" > ~/.streamlit/config.toml
